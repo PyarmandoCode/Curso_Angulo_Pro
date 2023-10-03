e5d7c1325882e59ffe61982cefb94ff335bf61b1
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-10-03 18:20:02 -0500

    Dia 2 Curso de Angular-Pro

--- app_dia02_angular/src/app/components/tareas/tareas.component.ts
+++ app_dia02_angular/src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+interface estructura_tareas {
+  tarea: string;
+  estado: string
+}
 
 @Component({
   selector: 'app-tareas',
@@ -14,6 +19,42 @@ export class TareasComponent {
   //   this.tareas.push(nuevaTarea);
   //   //console.log(this.tareas.push)
   // }
+  mistareas: estructura_tareas[] = [];
+
+  //Acceder a elementos del DOM
+  @ViewChild('todo') todo!: ElementRef;
+
+  addtodo(value: string):void {
+    var info: estructura_tareas = { tarea: value, estado: 'nocompletado' }
+    if (value !== "") {
+      this.mistareas.push(info);
+      //Limpiar el Valor de la Caja de Texto
+      this.todo.nativeElement.value = "";
+    }
+    else {
+      alert("Debe Ingresar una Tarea");
+    }
+
+  }
+
+  deleteitem(value: string):void {
+    for (let i = 0; i <= this.mistareas.length; i++) {
+      if (value == this.mistareas[i].tarea) {
+        this.mistareas.splice(i, 1)
+      }
+    }
+  }
+
+  completeitem(value:string):void {
+    for (let i = 0; i <= this.mistareas.length; i++) {
+      if (value == this.mistareas[i].tarea) {
+        this.mistareas[i].estado="completado"
+      }
+    }
+
+  }
+
+
   constructor() {
 
   }
